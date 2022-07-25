c8be85affc420d21bf3e1fe4fc82bb46cba63bcf
Silas Melo <silasmelo12@gmail.com>
2022-07-25 18:42:50 -0300

    Handler global

---
+++ src/main/java/projeto/springboot/handler/RestExceptionHandler.java
@@ -0,0 +1,25 @@
+package projeto.springboot.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import projeto.springboot.exception.BadRequestException;
+import projeto.springboot.exception.BadRequestExceptionDetails;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException bre){
+        return new ResponseEntity<>(
+                BadRequestExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request Exception, Check the Documentation")
+                        .details(bre.getMessage())
+                        .developerMessege(bre.getClass().getName())
+                        .build(),HttpStatus.BAD_REQUEST);
+    }
+}
