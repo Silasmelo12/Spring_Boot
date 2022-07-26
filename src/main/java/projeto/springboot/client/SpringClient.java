b1dbf9fd98db9f2dff8fdfb8d602b491b86d99a4
Silas Melo <silasmelo12@gmail.com>
2022-07-26 00:08:36 -0300

    RestTemplate getForObject e getForEntity

---
+++ src/main/java/projeto/springboot/client/SpringClient.java
@@ -0,0 +1,21 @@
+package projeto.springboot.client;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import projeto.springboot.domain.Anime;
+
+@Log4j2
+public class SpringClient {
+    public static void main(String[] args) {
+
+        ResponseEntity<Anime> entity = new RestTemplate().getForEntity
+                ("http://localhost:8080/animes/{id}", Anime.class,13);
+        log.info(entity);
+
+        Anime anime = new RestTemplate().getForObject("http://localhost:8080/animes/{id}", Anime.class,13);
+
+        log.info(anime);
+
+    }
+}
